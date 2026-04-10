69f446af5fbd8419b84aa75fab136de54eb7fc44
itajacy TI02 <itajacy@bsd.com.br>
2026-04-10 11:28:22 -0300

    aspectos basicos de uma funcao

---
+++ funcoes_dart/lib/funcoes_basicas.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  
+
+
+}
+
+// funcao com retorno
+int recuperarIdade() {
+  return 42;
+}
+
+// funcao sem retorno explicito (dynamic)
+recuperarNome() {}
+
+// funcao sem retorno (void)
+void recuperarNada(){}
