13d3e992d0a5e3d2fe26e46b6cf1a19aebcddb80
itajacy TI02 <itajacy@bsd.com.br>
2026-04-10 14:23:29 -0300

    funcoes arrow function

---
+++ funcoes_dart/lib/funcoes_arrow.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  somar(10,20 );
+
+ var subtrair = (int a, int b) => a-b;
+
+ subtrair(20,10);
+
+}
+
+// cuidado para não perder legibilidade
+
+int somar(int a, int b) => a+b;
