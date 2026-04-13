fc3ef29b997e51572e98379407c11bc6ec2ddf36
itajacy TI02 <itajacy@bsd.com.br>
2026-04-13 16:25:03 -0300

    manipulando listas

---
+++ listas_e_loops/lib/listas/exemplo_lista.dart
@@ -0,0 +1,23 @@
+void main() {
+
+  List<String> lista = [];
+  var lista0 = <String>[];
+  List<String?> lista2 = ['nome', null];
+  List<String>? lista3 = null;
+
+  List<String> lista5 = ['ana', 'saulo'];
+  print(lista5[1]);
+  List<String> lista6 = ['joana', 'paulo'];
+
+  lista5.add('Maria');
+  lista5.remove('ana');
+  lista5.removeAt(0);
+
+  print(lista5);
+
+  lista5.addAll(lista6);
+
+
+
+
+}
