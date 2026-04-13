8d9dbea058c94bc1cd23c8ff5a2ee081f3e07030
itajacy TI02 <itajacy@bsd.com.br>
2026-04-13 14:18:44 -0300

    modificador const

---
+++ modificadores_imutabilidade/lib/consts.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  
+  const double pi = 3.14159;  // tempo de compilação
+
+  final double piFinal;
+  piFinal = valorPi(); //  tempo de execução
+
+}
+double valorPi(){
+  return 3.1459;
+}
