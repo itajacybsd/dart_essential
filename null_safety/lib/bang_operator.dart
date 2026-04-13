cd61e3802305ab145d0cf8a36908d0bb4350d3f4
itajacy TI02 <itajacy@bsd.com.br>
2026-04-13 11:28:30 -0300

    null assert operator ou bang operator

---
+++ null_safety/lib/bang_operator.dart
@@ -0,0 +1,14 @@
+String? nomeGlobal;
+
+
+
+
+
+
+void imprimirNomeGlobal() {
+  if (nomeGlobal != null) {
+    // print((nomeGlobal as String).length);
+    print(nomeGlobal!.length); // a exclamação
+    
+  }
+}
