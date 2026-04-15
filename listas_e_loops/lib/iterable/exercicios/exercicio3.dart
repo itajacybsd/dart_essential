e07fbb38bfb351f7123d0cb336ed00f28cfc7971
itajacy TI02 <itajacy@bsd.com.br>
2026-04-15 11:13:46 -0300

    exercicio3 iterable

--- listas_e_loops/lib/iterable/exercicios/exercicio3.dart
+++ listas_e_loops/lib/iterable/exercicios/exercicio3.dart
@@ -6,4 +6,21 @@ onde o nome deve ser armazenado com a primeira letra maiúscula.
 
 void main(List<String> args) {
   
+List<String> nomes = ['jose', 'ana', 'paula'];
+
+List<User> usuarios;
+
+// var nomesPrimeiraMaiusc = nomes.map((nome) => nome.replaceFirst(nome.substring(0,1),nome.substring(0,1).toUpperCase() ));
+usuarios = nomes.map((nome) => User(nome: nome.replaceFirst(nome.substring(0,1),nome.substring(0,1).toUpperCase()))).toList();
+print(usuarios);
+
+
+}
+
+class User {
+  String? nome;
+  
+  User({this.nome});
+
+
 }
